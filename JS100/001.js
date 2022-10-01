26e14d393cf70772badea6aa90963fcc802bfffe
hayeonii <hayexn51@gmail.com>
2022-10-02 00:44:29 +0900

    10월 1일

---
+++ JS100/001.js
@@ -0,0 +1,21 @@
+var nums = [100, 200, 300, 400, 500];
+nums.pop();
+nums.pop();
+
+// nums.splice(3,2);
+
+console.log(nums);
+
+// 나는 pop 두번으로 구현함... 근데 splic가 더 좋았을 거 같다는 생각
+
+// 배열의 추가 / 삭제
+    // 추가
+        // 1. push
+        // 2. unshift
+        // 3. splice
+
+    // 삭제
+        // 1. pop
+        // 2. shift
+        // 3. splice
+        // 4. delete
