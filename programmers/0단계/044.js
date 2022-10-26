dd604bbcae8d164f7cd41bc436742b862740773d
hayeonii <hayexn51@gmail.com>
2022-10-26 22:22:56 +0900

    solve : 옷가게 할인 받기

---
+++ programmers/0단계/044.js
@@ -0,0 +1,8 @@
+// 옷가게 할인 받기
+
+function solution(price) {
+    if (price >= 500000) return Math.floor(price * 0.8);
+    if (price >= 300000) return Math.floor(price * 0.9);
+    if (price >= 100000) return Math.floor(price * 0.95);
+    if (price < 100000) return price
+}
