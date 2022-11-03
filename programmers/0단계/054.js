16b79d576cf765bf1951f162f49fbc1929b92a48
hayeonii <hayexn51@gmail.com>
2022-11-03 22:36:22 +0900

    Solve : 피자 나눠 먹기 (2)

---
+++ programmers/0단계/054.js
@@ -0,0 +1,11 @@
+// 피자 나눠 먹기 (2)
+
+function solution(n) {
+    const getLCM = (a, b) => {
+    let lcm = 1;
+    while ((lcm % a !== 0 || lcm % b !== 0) && lcm < a * b) lcm += 1;
+        return lcm;
+    };
+
+    return getLCM(n, 6) / 6;
+}
