16195d25e9ecc8532a0c4193b9c1808062676daf
hayeonii <hayexn51@gmail.com>
2023-02-03 17:21:53 +0900

    Solve: 정수 내림차순으로 배치하기

---
+++ programmers/1단계/010.js
@@ -0,0 +1,11 @@
+// 정수 내림차순으로 배치하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/12933
+
+function solution(n) {
+  return parseInt(
+    (n + "")
+      .split("")
+      .sort((a, b) => b - a)
+      .join("")
+  );
+}
