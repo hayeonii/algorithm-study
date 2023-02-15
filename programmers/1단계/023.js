0059e926866b06baf05b3a2e8f33dff9bed6064f
hayeonii <hayexn51@gmail.com>
2023-02-15 21:13:25 +0900

    Solve: 내적

---
+++ programmers/1단계/023.js
@@ -0,0 +1,11 @@
+// 내적
+// https://school.programmers.co.kr/learn/courses/30/lessons/70128
+
+function solution(a, b) {
+  var answer = 0;
+
+  for (i = 0; i < a.length; i++) {
+    answer += a[i] * b[i];
+  }
+  return answer;
+}
