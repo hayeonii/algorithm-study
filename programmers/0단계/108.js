354b5e235bdeb3db950c66c9a1fb812889df7da7
hayeonii <hayexn51@gmail.com>
2023-05-10 19:40:12 +0900

    Solve: 수 조작하기 1

---
+++ programmers/0단계/108.js
@@ -0,0 +1,19 @@
+// 수 조작하기 1
+// https://school.programmers.co.kr/learn/courses/30/lessons/181926
+
+function solution(n, control) {
+  return [...control].reduce((a, c) => {
+    switch (c) {
+      case "w":
+        return a + 1;
+      case "s":
+        return a - 1;
+      case "d":
+        return a + 10;
+      case "a":
+        return a - 10;
+      default:
+        return a;
+    }
+  }, n);
+}
