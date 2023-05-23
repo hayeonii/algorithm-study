94f7492bb9f3c0af46a1b89a22660ba4118dec3c
hayeonii <hayexn51@gmail.com>
2023-05-23 18:03:13 +0900

    Solve: 문자열 잘라서 정렬하기

---
+++ programmers/0단계/151.js
@@ -0,0 +1,9 @@
+// 문자열 잘라서 정렬하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/181866
+
+function solution(myString) {
+  return myString
+    .split("x")
+    .filter((v) => v !== "")
+    .sort();
+}
